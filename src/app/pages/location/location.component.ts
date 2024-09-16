701edb2bb47d72bbb4fcbf18d2f8036a62281119
Mirza Hamzic <mhamzic@gmail.com>
2024-09-16 08:23:46 +0200

    Correction to location-card dummy locations

--- src/app/pages/location/location.component.ts
+++ src/app/pages/location/location.component.ts
@@ -10,22 +10,25 @@ import { Location } from 'src/app/location-card/location-card.component';
   templateUrl: './location.component.html',
   styleUrls: ['./location.component.scss']
 })
+
+const DUMMY_LOCATIONS = [
+  {
+    id: 1,
+    name: 'Konjicki klub "Smet"',
+    address: 'Smetovi bb',
+    type: 'Zooloski vrt',
+    image: 'https://images.pexels.com/photos/808465/pexels-photo-808465.jpeg',
+  },
+  {
+    id: 2,
+    name: 'Restoran Smetovi',
+    address: 'Smetovi bb',
+    type: 'Restoran',
+    image:
+      'https://images.pexels.com/photos/552785/pexels-photo-552785.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+  },
+];
+
 export class LocationComponent {
-  locations: Location[] = [
-    {
-      id: 1,
-      name: 'Konjicki klub "Smet"',
-      address: 'Smetovi bb',
-      type: 'Zooloski vrt',
-      image: 'https://images.pexels.com/photos/808465/pexels-photo-808465.jpeg',
-    },
-    {
-      id: 2,
-      name: 'Restoran Smetovi',
-      address: 'Smetovi bb',
-      type: 'Restoran',
-      image:
-        'https://images.pexels.com/photos/552785/pexels-photo-552785.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    },
-  ];
+  locations: Location[] = DUMMY_LOCATIONS;
 }
