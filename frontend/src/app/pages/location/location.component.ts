2beaeaa45ed905edefb7ab077ba5ef6ceb255c58
Tarik Dedić <tarikdedic95@gmail.com>
2024-09-21 22:00:39 +0200

    Changes on UI home and some examples for backend creation of tables - first draft

--- frontend/src/app/pages/location/location.component.ts
+++ frontend/src/app/pages/location/location.component.ts
@@ -9,7 +9,7 @@ const DUMMY_LOCATIONS = [
     name: 'Konjicki klub "Smet"',
     address: 'Smetovi bb',
     type: 'Zooloski vrt',
-    image: 'https://images.pexels.com/photos/808465/pexels-photo-808465.jpeg',
+    image: '/assets/images/smet.jpg',
   },
   {
     id: 2,
@@ -17,7 +17,7 @@ const DUMMY_LOCATIONS = [
     address: 'Smetovi bb',
     type: 'Restoran',
     image:
-      'https://images.pexels.com/photos/552785/pexels-photo-552785.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+      '/assets/images/restoran_360.jpg',
   },
 ];
 
