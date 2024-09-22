ac844c216986f0bcb1dbb2e4f4b8cdb8573c1ee1
Tarik Dedić <tarikdedic95@gmail.com>
2024-09-22 12:08:04 +0200

    UI changes on home and location

--- frontend/src/app/pages/location/location.component.ts
+++ frontend/src/app/pages/location/location.component.ts
@@ -1,36 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LocationCardComponent } from 'src/app/components/location-card/location-card.component';
 import { Location } from 'src/app/components/location-card/location-card.component';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 const DUMMY_LOCATIONS = [
   {
     id: 1,
-    name: 'Konjicki klub "Smet"',
+    name: 'Konjički klub "Smet"',
     address: 'Smetovi bb',
     type: 'Zooloski vrt',
     image: '/assets/images/smet.jpg',
   },
   {
     id: 2,
-    name: 'Restoran Smetovi',
+    name: 'Restoran "960"',
     address: 'Smetovi bb',
     type: 'Restoran',
     image:
-      '/assets/images/restoran_360.jpg',
+      '/assets/images/restoran_960.jpg',
   },
 ];
 
 @Component({
   selector: 'app-location',
   standalone: true,
-  imports: [LocationCardComponent, CommonModule],
+  imports: [LocationCardComponent, CommonModule, GoogleMapsModule],
   templateUrl: './location.component.html',
   styleUrls: ['./location.component.scss']
 })
 
 
 
-export class LocationComponent {
+export class LocationComponent implements OnInit {
+  zoom = 12;
+  center: google.maps.LatLngLiteral = {
+    lat: 44.241962,
+    lng: 17.969424
+  };
+  options: google.maps.MapOptions = {
+    mapTypeId: 'hybrid',
+    zoomControl: false,
+    scrollwheel: false,
+    draggable: false,
+    disableDoubleClickZoom: true,
+    maxZoom: 20,
+    minZoom: 16,
+  }
   locations: Location[] = DUMMY_LOCATIONS;
+
+  ngOnInit() {}
+ 
+  zoomIn() {
+    if (this.zoom < 15) this.zoom++;
+  }
+ 
+  zoomOut() {
+    if (this.zoom > 8) this.zoom--;
+  }
 }
