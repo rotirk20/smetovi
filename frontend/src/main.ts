d9dbd94ea628c077dc597c8ed16ba77e44a7e788
Tarik Dedić <tarikdedic95@gmail.com>
2024-09-28 21:25:29 +0200

    Adding item-list component and UI improvements and getting mockup data from json

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,9 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app-routing.module';  // Import your routes
+import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes)  // Provide the routes
+    provideRouter(routes),
+    provideHttpClient(),
   ]
 }).catch(err => console.error(err));
