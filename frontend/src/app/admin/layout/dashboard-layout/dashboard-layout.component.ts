ee64350ccf8c769b38f550ca94685aa54f53198c
Tarik Dedić <tarikdedic95@gmail.com>
2024-10-19 00:02:46 +0200

    Making dashboard sidebar responsive and open/close, navbar submenu adjustments

--- frontend/src/app/admin/layout/dashboard-layout/dashboard-layout.component.ts
+++ frontend/src/app/admin/layout/dashboard-layout/dashboard-layout.component.ts
@@ -1,3 +1,4 @@
+import { NgClass, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { Router, NavigationEnd } from '@angular/router';
@@ -6,11 +7,13 @@ import { AuthService } from 'src/app/shared/services/auth.service';
 @Component({
   selector: 'app-dashboard-layout',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, NgClass, NgIf],
   templateUrl: './dashboard-layout.component.html',
   styleUrls: ['./dashboard-layout.component.scss'],
 })
 export class DashboardLayoutComponent implements OnInit {
+  sidebarOpen = true;  // Change this to false if you want the sidebar to start closed
+
   constructor(private router: Router, private authService: AuthService) {}
 
   ngOnInit() {
@@ -19,6 +22,10 @@ export class DashboardLayoutComponent implements OnInit {
         console.log('Navigated to:', event.url);
       }
     });
+  }
+
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
   }
 
   signOut() {
