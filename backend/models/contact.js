04e1bd2bad97343ed999a234d0bd1f759b55b909
Mirza Hamzic <mhamzic@gmail.com>
2024-10-18 09:30:50 +0200

    Contact sending fix and port changes

--- backend/models/contact.js
+++ backend/models/contact.js
@@ -4,10 +4,10 @@ const { sequelize } = require("../config/db");
 const Contact = sequelize.define(
   "Contact",
   {
-    name: { type: DataTypes.STRING, allowNull: false, unique: true },
-    contactInfo: { type: DataTypes.STRING, allowNull: false, unique: true },
-    subject: { type: DataTypes.STRING, allowNull: false, unique: true },
-    message: { type: DataTypes.STRING, allowNull: false, unique: true },
+    name: { type: DataTypes.STRING, allowNull: false},
+    contactInfo: { type: DataTypes.STRING, allowNull: false },
+    subject: { type: DataTypes.STRING, allowNull: false },
+    message: { type: DataTypes.STRING, allowNull: false },
   },
   { timestamps: true }
 );
